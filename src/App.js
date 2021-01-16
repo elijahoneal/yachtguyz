f69ec63691fb409b2d9c54b97bbebd77f717ce35
Elijah <elijahoneal@yahoo.com>
2021-01-16 12:59:48 -0500

    Boat List renders to DOM

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import { Jumbotron, Card, CardImg, CardText, CardBody,
 import './App.css';
 import logo from './images/yachtguyz.svg'
 import knottyLaneBG from './images/48aknottylane_1.jpg'
-
+import BoatList from './BoatList'
 
 const cardStyle = {
-  width:'33%',
-  backgroundColor:'grey'
+  backgroundColor:'yellow',
+  display: 'none'
 }
 function App() {
   return (
@@ -27,21 +27,36 @@ function App() {
         <p className="lead">Book the perfect yacht for your event!</p>
         <hr className="my-2" />
         <p>Yacht rentals are as simple as ever thanks to The Yacht Guyz. An assortment of vessels are at your disposal. Each equipped for small or large capacities.</p>
+        <p>Perfect for birthdays, anniversaries, fishing, or cruising on the water.</p>
         <p className="lead">
-          <Button color="primary">Learn More</Button>
+          <Button color="warning">Learn More</Button>
         </p>
       </Jumbotron>
         </section>
         <section className='yachts'>
-        <Card style = {cardStyle}>
-        <CardImg top width="100%" src={logo} alt="Card image cap" />
-        <CardBody>
-          <CardTitle tag="h5">Card title</CardTitle>
-          <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-          <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
+          {BoatList.map( boat => {
+            return(
+              <Card style = {cardStyle}>
+                <CardImg top width="100%" src={boat.image} alt="Card image cap" />
+                <CardBody>
+                  <CardTitle tag="h5">{boat.title}</CardTitle>
+                  <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
+                  <CardText>
+                      {boat.details.map( item =>{
+                        return(
+                          <ul>
+                          <li>{item}</li>
+                          {console.log(item)}
+                          </ul>
+                        )
+                      })}
+                    </CardText>
+                  <Button>Button</Button>
+                </CardBody>
+              </Card>
+            )
+          })}
+        
         </section>
       </main>
     </div>
---
+++ src/BoatList.js
@@ -0,0 +1,75 @@
+import naomi from './images/36naomi_3.jpg'
+const BoatList = [
+    {
+      id:'Naomi36ft',
+      title: '36ft yacht “Naomi”',
+      image: naomi,
+       details: [
+           
+                'Seats up to 10 Guest, plenty of seating on the hull and rear of the yacht.',
+                'Upgraded Bluetooth sound system.',
+                'Equipped with a restroom, bedroom, kitchen and dining room.',
+                'Captain, fuel, float mat to hold 6 people included.'
+              ]
+    },
+    {
+        id:'KnottyLane48ft',
+        title: '48ft yacht “Knotty Lane”',
+        image: naomi,
+         details: [
+             
+                'Seats up to 12 Guest, plenty of seating on the hull and rear of the yacht.',
+                'Upgraded Bluetooth sound system.',
+                'Equipped with a 2 restrooms, 2 bedrooms, kitchen and living room area.',
+                'Captain, fuel , Sea scooter & float mat to hold 10 people included.'
+                ]
+    },
+    {
+        id:'SeaRay55ft',
+        title: '55ft yacht "Sea Ray"',
+        image: naomi,
+         details: [
+             
+                'Seats up to 12 Guest, plenty of seating on the hull and rear of the yacht.',
+                'Upgraded Bluetooth sound system.',
+                'Equipped with a 2 restrooms, 2 bedrooms, kitchen and living room area.',
+                'Captain,fuel, & soft drinks included.'
+              ]
+    },
+    {
+        id:'Caprice130ft',
+        title: '130ft yacht "Caprice"',
+        image: naomi,
+         details: [
+             
+                'Accommodates 75-329 guests Per person meal and Drink packages.',
+                'Sit down dinner service for up to 160 guests.',
+                'Entire first deck available for dining.',
+                'Custom made copper ceiling.',
+                '1,700 Sq ft main salon w, Custom bar, baby grand piano and furniture groupings.',
+                'State of the art sound system.',
+                'DJ On Request.',
+                'Captain, Fuel, 3-6 Crew included.'
+               ]
+    },
+    {
+        id:'Catalina130ft',
+        title: '130ft yacht "Catalina"',
+        image: naomi,
+         details: [
+            
+                'Designed and appointed to accommodate larger group.',
+                'Accommodates up to 516 guests Per person meal and Drink packages.',
+                '2700 Sq Ft Dining room.',
+                'Dining on 1 level.',
+                'Top deck is partially enclosed and has a second full bar, elegant lighting and eight foot ceilings.',
+                'Top deck also has spacious outdoor area.', 
+                'State of the art sound system.', 
+                'DJ On Request.',
+                'Captain, Fuel, 3-6 Crew included.'
+               ]
+    }
+
+];
+
+export default BoatList;
