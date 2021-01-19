80fd5d77cc6c4c4fff96f5414541e2a2666b36be
Elijah <elijahoneal@yahoo.com>
2021-01-18 22:47:25 -0500

    Add Carousel + Card Anchors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Jumbotron, Card, CardImg, CardText, CardBody,
-  CardTitle, Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+  CardTitle } from 'reactstrap';
 import './App.css';
 import logo from './images/yachtguyz.svg'
 import knottyLaneBG from './images/48aknottylane_1.jpg'
@@ -9,68 +9,46 @@ import insta from './images/instagram.svg'
 import phone from './images/phone-icon.svg'
 import Carousel from './Carousel';
 
-
-const cardStyle = {
-  backgroundColor:'yellow',
-  
-}
 function App() {
-  const [modal, setModal] = useState(false);
-
-  const toggle = () => setModal(!modal);
+  
   return (
     <div className="App">
       <header className="App-header">
         <img className='header-bg' src={knottyLaneBG} alt='Knotty Lane Yacht Background'></img>
+
         <section className='launch'>
-        <img className='logo' src = {logo} alt = 'The YachtGuyz Gold Logo'></img>
-        <Button color= 'warning'>Set Sail Today</Button>
+          <img className='logo' src = {logo} alt = 'The YachtGuyz Gold Logo'></img>
+          <button>Set Sail Today</button>
         </section>
+
       </header>
       <main>
         <section className='intro'>
-        <Jumbotron>
-        <h1 className="display-3">Need A Yacht? Call Us, Why Not!</h1>
-        <p className="lead">Book the perfect yacht for your event!</p>
-        <hr className="my-2" />
-        <p>Yacht rentals are as simple as ever thanks to The Yacht Guyz. An assortment of vessels are at your disposal. Each equipped for small or large capacities.</p>
-        <p>Perfect for birthdays, anniversaries, fishing, or cruising on the water.</p>
-        <Carousel></Carousel>
+          <Jumbotron>
+          <h1 className="display-3">Need A Yacht? Call Us, Why Not!</h1>
+          <p className="lead">Book the perfect yacht for your event!</p>
+          <hr className="my-2" />
+          <p>Yacht rentals are as simple as ever thanks to The Yacht Guyz. An assortment of vessels are at your disposal. Each equipped for small or large capacities.</p>
+          <p>Perfect for birthdays, anniversaries, fishing, or cruising on the water.</p>
+          <Carousel></Carousel>
       </Jumbotron>
    
         </section>
         <section className='yachts'>
           {BoatList.map( boat => {
             return(
-              <Card style = {cardStyle}>
+              <Card>
                 <CardImg top width="100%" src={boat.image} alt="Card image cap" />
+
                 <CardBody>
-                  <CardTitle tag="h5">{boat.title}</CardTitle>
-                  
+                  <CardTitle tag="h5">{boat.title}</CardTitle>  
                   <CardText tag="ul">
-   
                       {boat.details.map( item =>{
                         return(
-                          
-                          <li>{item}</li>
-                          
+                          <li alt='Anchor by Aina from the Noun Project'>{item}</li>
                         )
                       })}
-           
                     </CardText>
-                    <div>
-                      <Button color="danger" onClick={toggle}>View Gallery</Button>
-                      <Modal isOpen={modal} toggle={toggle} className='gallery'>
-                        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-                        <ModalBody>
-   
-                        </ModalBody>
-                        <ModalFooter>
-                          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-                          <Button color="danger" onClick={toggle}>Cancel</Button>
-                        </ModalFooter>
-                      </Modal>
-                    </div>
                 </CardBody>
               </Card>
             )
@@ -81,20 +59,26 @@ function App() {
             <h2>Book the perfect yacht</h2>
             <p>Contact us today via email or phone to plan your next event.</p>
             <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify-recaptcha="true" data-netlify="true"  >
-                    <label class = "hidden">Don’t fill this out if you’re human: <input name="bot-field" /></label>
+                    <label className = "hidden">Don’t fill this out if you’re human: <input name="bot-field" /></label>
                     <label htmlFor="name">Name</label>
                     <input name="name" id= "name" type="text" placeholder="Your Name"/>
                     <label htmlFor="email">Email</label>
                     <input name="email" id= "email" type="text" placeholder="Your emailname@business.com"/>
                     <label htmlFor="message">Message</label>
                     <textarea name="message" id="message"></textarea>
-                    <div data-netlify-recaptcha="true"></div>
                     <button type = "submit">Submit</button>
                 </form>
-                <img src = {insta} alt="https://www.instagram.com/theyachtguyz/"></img><span>@theyachtguyz</span>
-                <img src = {phone} alt='Phone by Iconographer from the Noun Project'></img><span>954-591-8389</span>
+               
         </article>
       </main>
+      <footer>
+        <div className='insta'>
+            <img src = {insta} alt="https://www.instagram.com/theyachtguyz/"></img><span>@theyachtguyz</span>
+        </div>
+        <div className ='phone'>
+            <img src = {phone} alt='Phone by Iconographer from the Noun Project'></img><span>954-591-8389</span>
+        </div>
+      </footer>
     </div>
   );
 }
---
+++ src/Gallery.js
@@ -0,0 +1,226 @@
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+import naomi1 from './images/gallery/naomi_1.jpg'
+import naomi2 from './images/gallery/naomi_2.jpg'
+import naomi3 from './images/gallery/naomi_3.jpg'
+import naomi4 from './images/gallery/naomi_4.jpg'
+import naomi5 from './images/gallery/naomi_5.jpg'
+import knotty1 from './images/gallery/aknottylane_1.jpg'
+import knotty2 from './images/gallery/aknottylane_2.jpg'
+import knotty3 from './images/gallery/aknottylane_3.jpg'
+import knotty4 from './images/gallery/aknottylane_4.jpg'
+import knotty5 from './images/gallery/aknottylane_5.jpg'
+import searay1 from './images/gallery/55ftsearay_1.jpg'
+import searay2 from './images/gallery/55ftsearay_2.jpg'
+import searay3 from './images/gallery/55ftsearay_3.jpg'
+import searay4 from './images/gallery/55ftsearay_4.jpg'
+import searay5 from './images/gallery/55ftsearay_5.jpg'
+import caprice1 from './images/gallery/caprice_1.jpg'
+import caprice2 from './images/gallery/caprice_2.jpg'
+import caprice3 from './images/gallery/caprice_3.jpg'
+import catalina1 from './images/gallery/catalina_1.jpg'
+import catalina2 from './images/gallery/catalina_2.jpg'
+import catalina3 from './images/gallery/catalina_3.jpg'
+import catalina4 from './images/gallery/catalina_4.jpg'
+
+
+
+const items = [
+{
+   gallery:
+       [ {
+            src: naomi1,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            src: naomi2,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            src: naomi3,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            src: naomi4,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },
+          { 
+            src: naomi5,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },
+        ]
+},
+{        
+    gallery:[ {
+            src: knotty1,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            src: knotty2,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            src: knotty3,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            src: knotty4,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },
+          { 
+            src: knotty5,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },]
+   },
+    
+{     gallery: [
+     {
+            id:'Naomi36ft',
+            src: searay1,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            id:'imgsKnottyLane48ft',
+            src: searay2,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            id:'imgsSeaRay55ft',
+            src: searay3,
+            altText: '48ft yacht “Knotty Lane”',
+            caption: '48ft yacht “Knotty Lane”'
+          },
+          { 
+            id:'imgsCaprice130ft',
+            src: searay4,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },
+          { 
+            id:'imgsCatalina130ft',
+            src: searay5,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          },]
+   },
+  
+{    gallery: [   {
+            id:'Naomi36ft',
+            src: caprice1,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            id:'imgsKnottyLane48ft',
+            src: caprice2,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            id:'imgsSeaRay55ft',
+            src: caprice3,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },]
+  },
+  {    gallery:  [ {
+            id:'Naomi36ft',
+            src: catalina1,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            id:'imgsKnottyLane48ft',
+            src: catalina2,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            id:'imgsSeaRay55ft',
+            src: catalina3,
+            altText: '36ft yacht "Naomi"',
+            caption: '36ft yacht "Naomi"'
+          },
+          { 
+            id:'imgsCaprice130ft',
+            src: catalina4,
+            altText: 'Slide 3',
+            caption: 'Slide 3'
+          }]
+    }
+    ]
+
+
+
+
+const Gallery = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((boat) => {
+
+    return (
+       
+        <CarouselItem
+                onExiting={() => setAnimating(true)}
+                onExited={() => setAnimating(false)}
+                key={boat.src}
+              >
+                <img src={boat.gallery.src} alt={boat.gallery.altText} />
+                <CarouselCaption captionText={boat.gallery.caption} captionHeader={boat.gallery.caption} />
+              </CarouselItem>
+     
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
+}
+
+export default Gallery;
+
